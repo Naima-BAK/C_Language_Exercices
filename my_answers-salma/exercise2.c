42632289212938d842bb33138eefd0e73fba3574
salma fadili <salmafadili003@gmail.com>
2023-03-05 13:48:27 +0100

    answering exercise1

---
+++ my_answers-salma/exercise2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+    int main () {
+    int x;
+        printf("please enter num1: ");
+              scanf("%d",&x);
+                   printf("x=%d\n",x);
+    int y;
+        printf("please enter num2: ");
+              scanf("%d",&y);
+                   printf("y=%d\n",y);
+
+
+				 int result = x*y ;
+
+        printf("the result of multiplication of %d and %d is : %d ",x ,y ,result);
+               return 0;
+}
