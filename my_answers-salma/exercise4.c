74b5e51d16d0442dbd42b7d569685ff11cfff1e3
salma fadili <salmafadili003@gmail.com>
2023-03-06 01:42:52 +0100

    i solved the exercises

---
+++ my_answers-salma/exercise4.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+    int main()
+    {
+             int answer;
+               printf("how much is 2 times 2?");
+                   scanf("%i",&answer);
+     if (answer == 4)
+	 {printf("That's right!\n");
+	 }
+       else {
+      printf("incorrect answer . Please try again.\n");
+
+                   printf("how much is 2 times 2?");
+                     scanf("%i",&answer);
+                        printf("That's right!\n");
+       }
+    return 0;
+
+
+
+
+
+
+
+ } 
