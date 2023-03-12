abc92d12ddf3957ac5864e350fe1d07a1abfd372
salma fadili <salmafadili003@gmail.com>
2023-03-12 14:00:39 +0100

    ex area of rectangle done

---
+++ my_answers-salma/area-rectangle.c
@@ -0,0 +1,16 @@
+/* area of rectangle */
+#include <stdio.h>
+  int main()
+  { 
+    float L;
+       printf("enter the length of rectangle:");
+    	scanf("%f",&L);
+   	float W;
+      printf("enter the width of rectangle:");
+	    scanf("%f",&W);
+		float A = L * W ;
+		 printf("the area of rectangle is %f sq.units\n",A);
+		 return 0;
+		 }
+
+
