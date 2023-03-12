f07400ee5d214c13afea1b5e4e64ee8b5ae358a9
salma fadili <salmafadili003@gmail.com>
2023-03-12 13:30:56 +0100

    ex leapyear done

---
+++ my_answers-salma/leapyear.c
@@ -0,0 +1,41 @@
+/* c program to check Leap year */
+#include<stdio.h>
+int main()
+{
+    int y;
+    printf("enter a year please:");
+	scanf("%d",&y);
+      if (y % 4 ==0)  {
+	      if (y % 100 == 0){
+	          if (y % 400 == 0) {
+			        printf("%d is a leap year\n",y);
+					}
+		            else { 
+					       printf("%d is not a leap year\n",y);
+		           	}
+	     	}
+             else {
+				  printf("%d is a leap year\n",y);
+	        }  
+          }
+		  else {
+		        printf("%d is not a leap year\n",y);
+          }
+
+
+ 
+
+  return 0;
+
+
+
+
+
+
+
+
+
+
+
+
+}
